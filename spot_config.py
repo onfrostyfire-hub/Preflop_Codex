fb5ba5a0c21b0ca1eed1aad27cbfa44fc9bd7519
onfrostyfire-hub <onfrostyfire@gmail.com>
2026-02-19 06:25:12 -0300

    Update spot_config.py

--- spot_config.py
+++ spot_config.py
@@ -23,6 +23,16 @@ SPOT_CONFIGS = {
     "EP vs 3bet MP": SpotConfig(hero="EP", villain="MP", dealer="BTN", hero_bet=2.5, villain_bet=7.5),
     "EP vs 3bet CO/BU": SpotConfig(hero="EP", villain="CO", dealer="BTN", hero_bet=2.5, villain_bet=7.5),
     "EP vs 3bet Blinds": SpotConfig(hero="EP", villain="SB", dealer="BTN", hero_bet=2.5, villain_bet=10.0),
+
+    "CO def vs 3bet BU": SpotConfig(hero="CO", villain="BTN", dealer="SB", hero_bet=2.5, villain_bet=7.5),
+    "CO def vs 3bet SB": SpotConfig(hero="CO", villain="SB", dealer="BTN", hero_bet=2.5, villain_bet=12),
+    "CO def vs 3bet BB": SpotConfig(hero="CO", villain="BB", dealer="BTN", hero_bet=2.5, villain_bet=12),
+    "BU def vs 3bet SB": SpotConfig(hero="BTN", villain="SB", dealer="BTN", hero_bet=2.5, villain_bet=12),
+    "BU def vs 3bet BB": SpotConfig(hero="BTN", villain="BB", dealer="BTN", hero_bet=2.5, villain_bet=12),
+    "SB def vs 3bet BB": SpotConfig(hero="SB", villain="BB", dealer="BTN", hero_bet=3.0, villain_bet=9),
+    "BBvsBU x2.5 nl500": SpotConfig(hero="BB", villain="BTN", dealer="BTN", hero_bet=1.0, villain_bet=2.5),
+    "BBvsBU x2.5 nl100": SpotConfig(hero="BB", villain="BTN", dealer="BTN", hero_bet=1.0, villain_bet=2.5),
+
 }
 
 def get_spot_config(spot_name: str) -> SpotConfig:
