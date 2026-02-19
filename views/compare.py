24cc169bbcbcf5e6cdff59c12b449d7a4a923bb9
onfrostyfire-hub <onfrostyfire@gmail.com>
2026-02-19 06:17:00 -0300

    Update compare.py

--- views/compare.py
+++ views/compare.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import utils
 
+def show():
+    st.subheader("Range Lab")
+    db = utils.load_ranges()
+    if not db:
+        st.warning("Нет ренджей")
+        return
+
+    source = st.selectbox("Source", list(db.keys()))
+    scenario = st.selectbox("Scenario", list(db[source].keys()))
+    spot = st.selectbox("Spot", list(db[source][scenario].keys()))
+    data = db[source][scenario][spot]
+
+    st.write("### Данные спота")
+    st.json(data)
